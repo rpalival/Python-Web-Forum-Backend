6d6c22595c185c7b09a6e119fb59b2941220f514
rpalival <112983661+rpalival@users.noreply.github.com>
2023-12-06 15:20:28 -0500

    post

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
-from secrets import randbelow
+from flask import Flask, request, jsonify
+import secrets
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.get("/random/<int:sides>")
-def roll(sides):
-    if sides <= 0:
-        return {'err': 'need a positive number of sides' }, 400
-    return {'num': randbelow(sides) + 1}
+# In-memory storage for posts
+posts = {}
+post_counter = 0
+
+# Endpoint to create a post
+@app.route('/post', methods=['POST'])
+def create_post():
+    global post_counter
+    try:
+        # Parse and validate the JSON request
+        data = request.get_json()
+        if not data or 'msg' not in data or not isinstance(data['msg'], str):
+            return jsonify({'err': 'Bad request'}), 400
+
+        # Create a new post
+        post_id = post_counter
+        post_counter += 1
+        key = secrets.token_urlsafe(16)  # Generate a secure random key
+        timestamp = datetime.utcnow().isoformat()  # ISO 8601 timestamp in UTC
+
+        # Store the post
+        posts[post_id] = {'id': post_id, 'key': key, 'timestamp': timestamp, 'msg': data['msg']}
+
+        # Return the post data
+        return jsonify({'id': post_id, 'key': key, 'timestamp': timestamp}), 201
+
+    except Exception as e:
+        # Handle unexpected errors
+        return jsonify({'err': f'Unexpected error: {str(e)}'}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
