bc04aa1a5c9a146d0c3e64245fdb3d7ab4931eb0
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2023-12-17 20:06:13 -0500

    fix

--- test.sh
+++ test.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-./setup.sh
-
 set -e # exit immediately if newman complains
 trap 'kill $PID' EXIT # kill the server on exit
 
 ./run.sh &
 PID=$! # record the PID
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+nvm install node  # Install the latest version of Node.js and npm
 
 newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
 newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
