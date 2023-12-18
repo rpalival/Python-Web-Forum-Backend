0b8127a85a9aaac15354cace1ca3b21a0862e253
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2023-12-17 19:34:04 -0500

    fix

--- test.sh
+++ test.sh
@@ -5,9 +5,15 @@ trap 'kill $PID' EXIT # kill the server on exit
 
 ./run.sh &
 PID=$! # record the PID
+
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+
+
 # npm cache clean --force
 # npm uninstall -g newman
-npm install -g npm@latest
+# npm install -g npm@latest
 # npm install -g newman
 # newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
 # newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
