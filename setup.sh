d590ba686ab53a54e30803a376f74fac537c7db4
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2023-12-17 19:55:59 -0500

    fix

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
-nvm install node  # Install the latest version of Node.js and npm
 
 # npm install -g newman # Install the latest version of newman
 
