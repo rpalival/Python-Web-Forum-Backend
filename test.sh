2a183340ad95129b8bc0bc86a380da63fb4ded68
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2023-12-17 19:30:49 -0500

    fix

--- test.sh
+++ test.sh
@@ -8,6 +8,6 @@ PID=$! # record the PID
 # npm cache clean --force
 # npm uninstall -g newman
 npm install -g npm@latest
-npm install -g newman
-newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
-newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
+# npm install -g newman
+# newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
+# newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
