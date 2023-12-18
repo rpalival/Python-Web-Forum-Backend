daf0020297ac589bb4e79d4cbae6e7bde3428dd9
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2023-12-17 19:37:33 -0500

    fix

--- test.sh
+++ test.sh
@@ -17,7 +17,7 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 
 # npm cache clean --force
 # npm uninstall -g newman
-# npm install -g npm@latest
+npm install -g npm@latest
 # npm install -g newman
 # newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
 # newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
