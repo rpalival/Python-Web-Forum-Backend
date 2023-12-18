34593bf65a35f66617aa971f936b0673f6684aad
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2023-12-17 20:02:21 -0500

    fix

--- test.sh
+++ test.sh
@@ -6,5 +6,7 @@ trap 'kill $PID' EXIT # kill the server on exit
 ./run.sh &
 PID=$! # record the PID
 
+./setup.sh
+
 newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
 newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
